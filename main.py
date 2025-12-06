36482c38ef7d796493617649e15dd2400c780a24
Kajus Dobrovolskis <kajdob@ktu.lt>
2025-12-06 11:48:26 +0200

    bar ir pie chartai tos islaidos aisku veliau tures buti nuskaitomos nuo pagrindinio ui ar kazkas tokio

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-hello
+import numpy as np
+import matplotlib.pyplot as plt
 
-ben
+    
+pavadinimas=['Maistas','Transportas','Pramogos']
+islaidos=[270, 130, 85]
+
+def bar(pavadinimas, islaidos):
+
+    y=range(len(islaidos))
+    plt.bar(y, islaidos, color='green')
+    plt.xticks(y, pavadinimas)
+    plt.ylabel('Iðlaidos (EUR)')
+    plt.title('Iðlaidos pagal kategorijas')
+    
+    plt.show()
+
+def pie(pavadinimas, islaidos):
+  
+    plt.pie(islaidos, labels=pavadinimas, autopct='%1.1f%%', startangle=140,shadow=True)
+    plt.title('Iðlaidos pagal kategorijas')
+    
+    plt.show()
