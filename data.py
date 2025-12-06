3652d1d5a2ae803b4eb3f8d176a8f42ac2e79dcf
Dominykas Vnęk <domvne@ktu.lt>
2025-12-06 12:31:05 +0300

    added data.py ir cia bus funkcijos bus galima naudoti per GUI (kolkas nieko nedaro)

---
+++ data.py
@@ -0,0 +1,23 @@
+DATA = 'data.json'
+
+kategorijos=['Maistas','Transportas','Laisvalaikis ir pramogos','Kitos išlaidos']
+
+def load_data():
+    pass
+
+def save_data(data):
+    pass
+
+def add_pajamos(pajamos,kategorija):
+    pass
+
+def add_islaidos(islaidos,kategorija, daiktopavadinimas, menesis):
+    pass
+
+def islaidos_pagal_kategorija():
+    pass
+
+def menesiototal():
+    pass
+
+
