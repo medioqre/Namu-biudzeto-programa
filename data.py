d33ca0d44df2e2304051393140ededa37a632137
Dominykas Vnęk <domvne@ktu.lt>
2025-12-07 19:40:29 +0200

    pakeiciau truputi kad lengviau skaitytusi

--- data.py
+++ data.py
@@ -27,12 +27,12 @@ def add_islaidos():
 def islaidos_pagal_kategorija():
     kategorija=add_islaidos()[0]
     islaidos_suma=add_islaidos()[1]
-    kategorijos_suma={}
+    kategoriju_sumos={}
     for kateg, islaidos in zip(kategorija,islaidos_suma): #zip susieja 2 list pvz [1,2,3] ir antras list ['a','b','c'], tai zip(pirmaslist,antraslist) output padaro 1 a, 2 b, 3 c  (ne kableliais o nauja line)
-        if kateg not in kategorijos_suma:
-            kategorijos_suma[kateg]=0
-        kategorijos_suma[kateg]+=islaidos
-    return kategorijos_suma
+        if kateg not in kategoriju_sumos:
+            kategoriju_sumos[kateg]=0
+        kategoriju_sumos[kateg]+=islaidos
+    return kategoriju_sumos
     #print(kategorijos_suma)
 
 def menesiototal():
@@ -42,7 +42,8 @@ def menesiototal():
         laikas=i["laikas"] #duoda pvz '2025-10-20'
         menesis=laikas[:7] # palieka tik '2025-10'
         islaidos=i['money'] #duoda tos dienos islaidos eur
-        menesio_suma[menesis] = menesio_suma.get(menesis,0)+islaidos #
+        menesio_suma[menesis] = menesio_suma.get(menesis,0)+islaidos #.get sako -> jei menesio_suma[menesis] egzistuoja, tai naudoti jo reiksme, o jeigu ne tai naudoti 0. pvz menesis='2025-05' ir kolkas nera jo menesio_suma={} zodyne, tai bus menesio_suma['2025-05']=0+islaidos
+
     #print(menesio_suma)
     return menesio_suma
 
